853e2062f78d53cc0113893ff88a326c2faef407
aryansingh-0 <aryan03aryansingh@gmail.com>
2025-07-14 01:06:56 +0530

    update

--- components/ads/RightAds.js
+++ components/ads/RightAds.js
@@ -1,8 +1,7 @@
 'use client'
 // components/RightAds.js
 import { useEffect } from 'react';
-import Script from 'next/script';
-
+ 
 const RightAds = () => {
   useEffect(() => {
     try {
@@ -18,21 +17,14 @@ const RightAds = () => {
   return (
     <>
       {/* Load the AdSense script globally */}
-      <Script
-        async
-        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2404358914933411"
-        crossOrigin="anonymous"
-        strategy="lazyOnload" // Or "afterInteractive" depending on your preference
-      />
-
-      {/* The ad unit */}
-      <ins
-        className="adsbygoogle"
-        style={{ display: 'block' }}
-        data-ad-format="autorelaxed"
-        data-ad-client="ca-pub-2404358914933411"
-        data-ad-slot="4391646852"
-      ></ins>
+      <amp-ad width="100%" height="100%"
+     type="adsense"
+     data-ad-client="ca-pub-2404358914933411"
+     data-ad-slot="4391646852"
+     data-auto-format="mcrspv"
+     data-full-width="">
+  <div overflow=""></div>
+</amp-ad>
     </>
   );
 };
